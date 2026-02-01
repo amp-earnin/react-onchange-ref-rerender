f10b75600ca951457f50e9de2602ca9efc07e554
amp-earnin <amp.tassana@earnin.com>
2026-02-02 01:10:25 +0700

    Count execution times

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,22 +1,27 @@
-import { forwardRef, useEffect } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 
 type Props = {
   onChange?: (value: string) => void
 }
 
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const [executionCount, setExecutionCount] = useState<number>(0)
   console.log('render: Input')
   useEffect(() => {
-    console.log('onChange reference changed!')
+    setExecutionCount((prev) => prev + 1)
   }, [props.onChange])
 
   return (
-    <input
-      type="text"
-      placeholder="input with expensive onChange effect"
-      onInput={(event) => props.onChange?.(event.currentTarget.value)}
-      ref={ref}
-    />
+    <>
+      <input
+        type="text"
+        placeholder="input with expensive onChange effect"
+        onInput={(event) => props.onChange?.(event.currentTarget.value)}
+        ref={ref}
+      />
+      <span>{executionCount}</span>
+    </>
+
   );
 });
 
