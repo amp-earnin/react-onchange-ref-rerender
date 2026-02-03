2958e77cc52bf37b04446ea8102eb462ea7e3a5f
amp-earnin <amp.tassana@earnin.com>
2026-02-03 14:02:36 +0700

    Update onChange without trigger effect

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,16 +1,23 @@
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useEffect, useLayoutEffect, useRef, useState } from 'react';
 
 type Props = {
   onChange?: (value: string) => void
 }
 
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const onChangeRef = useRef(props.onChange)
   const [executionCount, setExecutionCount] = useState<number>(0)
+
+  // useLayoutEffect makes sure we have latest handler before any effects run
+  useLayoutEffect(() => {
+    onChangeRef.current = props.onChange
+  })
+
   // since props.onChange ref change on each render,
   // effect will be executed on each render as well
   useEffect(() => {
     setExecutionCount((prev) => prev + 1)
-  }, [props.onChange])
+  }, [])
 
   return (
     <>
